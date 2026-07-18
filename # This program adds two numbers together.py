5c0ea63abcea75b2c2c89e360ae32de5f4fefa0f
MD. SHORIF UDDIN <shorifuddin64662@gmail.com>
2026-07-18 19:22:17 +0600

    # This program adds two numbers together.py

---
+++ # This program adds two numbers together.py
@@ -0,0 +1,9 @@
+# This program adds two numbers together
+num1 = float(input("Enter the first number: "))
+num2 = float(input("Enter the second number: "))
+
+# Calculating the sum
+result = num1 + num2
+
+# Displaying the result
+print("The sum of the two numbers is:", result)
