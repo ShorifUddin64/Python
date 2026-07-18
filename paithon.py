1117580247b9eacce585e83a36c423ad4e4b446e
MD. SHORIF UDDIN <shorifuddin64662@gmail.com>
2026-07-18 19:13:38 +0600

    paithon.py

---
+++ paithon.py
@@ -0,0 +1,4 @@
+Python 3.14.6 (tags/v3.14.6:c63aec6, Jun 10 2026, 10:26:10) [MSC v.1944 64 bit (AMD64)] on win32
+Enter "help" below or click "Help" above for more information.
+>>> name = input("আপনার নাম লিখুন: ")
+... print("পাইথন প্রোগ্রামিংয়ে আপনাকে স্বাগতম, " + name + "!")
